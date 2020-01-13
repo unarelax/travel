ee379f99665283e75a143ea98695752f74568aa4
una <1048680895@qq.com>
2020-01-13 23:32:59 +0800

    首页头部95%

--- src/views/components/Header.vue
+++ src/views/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">内容</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe618;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe60c;</span>
+      请输入你要搜索的城市/地点
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe608;</span>
+    </div>
   </div>
 </template>
 
@@ -13,18 +21,34 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import "~@/assets/styles/varibles.styl"
 .header
   display: flex
-  line-height: .86rem
-  background: #004643
-  color: #abd1c6
-  .headre-left
-    width: .64rem
+  height: 0.86rem
+  line-height: 0.86rem
+  background: $bgColor
+  color: $nextGreen
+  .header-left
+    width: 0.64rem
     float: left
+    .back-icon
+      display: block
+      text-align: center
+      font-size: .4rem
   .header-input
     flex: 1
+    height: .64rem
+    line-height: .64rem
+    padding-left: .2rem
+    margin-top: .12rem
+    margin-left: .2rem
+    background: $nextGreen
+    border-radius: .1rem
+    color: $moreGreen
   .header-right
-    width:  1.24rem
+    width: 1.24rem
     float: right
     text-align: center
+    .arrow-icon
+      margin-left: -0.08rem
 </style>
