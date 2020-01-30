1f6754d47eb9312f8b0affacf127232536287f13
una <1048680895@qq.com>
2020-01-31 01:00:56 +0800

    finsh pageCity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from "../components/home/icons";
 import HomeRecommend from "../components/home/recommend";
 import HomeWeekend from "../components/home/weekend";
 import axios from "axios";
+import { mapState } from "vuex";
 export default {
   name: "home",
   components: {
@@ -26,15 +27,19 @@ export default {
   },
   data() {
     return {
+      lastCity: "",
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     };
   },
+  computed: {
+    ...mapState(["city"])
+  },
   methods: {
     getHomeInfo() {
-      axios.get("/api/index.json").then(this.getHomeInfoSucc);
+      axios.get("/api/index.json?city=" + this.city).then(this.getHomeInfoSucc);
     },
     getHomeInfoSucc(res) {
       res = res.data;
@@ -48,7 +53,14 @@ export default {
     }
   },
   mounted() {
+    this.lastCity = this.city;
     this.getHomeInfo();
+  },
+  activated() {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city;
+      this.getHomeInfo();
+    }
   }
 };
 </script>
