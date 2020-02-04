87acdfc13224f1192e34fcc278604cfb98728c0a
una <1048680895@qq.com>
2020-02-04 22:15:54 +0800

    list1/2

---
+++ src/components/detail/List.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="list">
+    <div class="item" v-for="(item, index) of list" :key="index">
+      <div class="item-title">
+        <span class="item-title-icon"></span>
+        {{ item.title }}
+      </div>
+      <div v-if="item.children" class="item-children">
+        <!-- 递归 -->
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailList",
+  props: {
+    list: Array
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import "~@/assets/styles/varibles.styl"
+.item-title
+  line-height: .8rem
+  font-size .32rem
+  padding 0 .2rem
+  .item-title-icon
+    position relative
+    top .12rem
+    display inline-block
+    width .36rem
+    height .36rem
+    background: url(../../../public/images/bill.png) no-repeat;
+.item-children
+  padding 0 .2rem
+</style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,18 +2,65 @@
   <div class="detail">
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from "../components/detail/Banner";
 import DetailHeader from "../components/detail/Header";
+import DetailList from "../components/detail/List";
 export default {
   name: "Detail",
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      list: [
+        {
+          title: "成人票",
+          children: [
+            {
+              title: "成人三馆联票",
+              children: [
+                {
+                  title: "成人三馆联票 ---某连锁店销售"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          title: "学生票",
+          children: [
+            {
+              title: "成人三馆联票"
+            }
+          ]
+        },
+        {
+          title: "儿童票",
+          children: [
+            {
+              title: "成人三馆联票"
+            }
+          ]
+        },
+        {
+          title: "特惠票",
+          children: [
+            {
+              title: "成人三馆联票"
+            }
+          ]
+        }
+      ]
+    };
   }
 };
 </script>
